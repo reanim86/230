bca240c84f955bcbe93f4a4523a823c82ecbf9ec
Aleksei Kudinov <reanim86@yandex.ru>
2024-12-13 16:52:20 +0300

    add connection to DB

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
-import csv
-
+import psycopg
+from pprint import pprint
 
 def get_data():
     """
@@ -18,3 +18,17 @@ def get_data():
             }
     response = requests.get('https://smsc.ru/sys/get.php', params=params)
     return response.json()
+
+data = get_data()
+pprint(data[0])
+
+with psycopg.connect(dbname='sms', user='postgres', password='postgres') as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+        INSERT INTO sms(id_smsc, date_create, tel, mccmnc, operator, description, name_sender, quantity, ip, status)
+        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+        """, data[0]['id'], data[0]['last_date'], data[0]['phone'], data[0]['mccmnc'], data[0]['operator'], data[0]['message'], data[0]['sender_id'])
+        conn.commit()
+conn.close()
+
+
